a132a6fdf33d0b1df63d686bc25d6b00ae151809
zainabFarih <zainab_farih@um5.ac.ma>
2026-06-01 23:19:24 +0100

    fix: enable SSL for Redis connection by leveraging Spring Boot auto-configuration

--- services/job-service/src/main/java/com/bloom/jobservice/config/RedisConfig.java
+++ services/job-service/src/main/java/com/bloom/jobservice/config/RedisConfig.java
@@ -1,61 +1,19 @@
 package com.bloom.jobservice.config;
 
 import com.bloom.jobservice.dto.JobResult;
-import io.lettuce.core.RedisURI;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
-import java.time.Duration;
 import java.util.List;
 
 @Configuration
 public class RedisConfig {
-
-    @Value("${spring.data.redis.url}")
-    private String redisUrl;
-
-    @Bean
-    @ConditionalOnMissingBean(RedisConnectionFactory.class)
-    @ConditionalOnProperty(name = "redis.ssl.enabled", havingValue = "true")
-    public RedisConnectionFactory redisConnectionFactory(
-            @Value("${spring.data.redis.timeout}") Duration commandTimeout) {
-
-        RedisURI uri = RedisURI.create(redisUrl);
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .commandTimeout(commandTimeout)
-                .useSsl()
-                .build();
-        RedisStandaloneConfiguration standalone =
-                new RedisStandaloneConfiguration(uri.getHost(), uri.getPort());
-        standalone.setUsername(uri.getUsername());
-        standalone.setPassword(new String(uri.getPassword()));
-        return new LettuceConnectionFactory(standalone, clientConfig);
-    }
-
-
-    @Bean
-    @ConditionalOnMissingBean(RedisConnectionFactory.class)
-    @ConditionalOnProperty(name = "redis.ssl.enabled", havingValue = "false",
-            matchIfMissing = true)
-    public RedisConnectionFactory redisConnectionFactoryNoSsl() {
-        RedisURI uri = RedisURI.create(redisUrl);
-        RedisStandaloneConfiguration standalone =
-                new RedisStandaloneConfiguration(uri.getHost(), uri.getPort());
-        return new LettuceConnectionFactory(standalone,
-                LettuceClientConfiguration.defaultConfiguration());
-    }
-
 
     @SuppressWarnings("unchecked")
     @Bean("jobsRedisTemplate")
